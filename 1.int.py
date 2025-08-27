468bb679915990453c2ee09e085171161a32adf9
Ajaykumar <pogulaajaykumar6801@gmail.com>
2025-08-27 14:11:57 +0530

    Add files via upload

---
+++ 1.int.py
@@ -0,0 +1,93 @@
+# 1) Decimal Number System Data 
+
+# The Decimal Number System  contains the following
+# Digits:  0  1   2   3  4  5   6   7  8  9 -----------Total Digits: 10
+# Base : 10
+
+
+# 2. Binary Number System Data
+
+a=0b1001
+print(a,type(a))
+
+bin(9)
+
+a=0b1111
+print(a,type(a))
+
+print(0b10000)
+
+a=0B0100
+print(a,type(a))
+
+
+# 3) Octal Number System data
+
+
+a=0o17
+print(a,type(a))
+
+oct(15)
+
+a=0O123
+print(a,type(a))
+
+oct(83)
+
+
+
+# e) Hexa Decimal Number System
+
+
+
+a=0xAC
+print(a,type(a))
+
+a=0XBEE
+print(a,type(a))
+
+hex(3054)
+
+hex(64206)
+
+b=0XFaCe
+print(b,type(b))
+
+a=0xACC
+print(a,type(a))
+
+# ---------------------------------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
