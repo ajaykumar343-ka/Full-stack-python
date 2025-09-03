73b91062d0f10e084230ec87a9a2f650dc48230c
Ajaykumar <pogulaajaykumar6801@gmail.com>
2025-09-03 20:32:30 +0530

    Add files via upload

---
+++ approches_of_python.py
@@ -0,0 +1,82 @@
+
+# 1. By using Interactive Approach
+
+print("Hello, World!")
+
+a = 5
+b = 10
+c = a + b
+print(c)
+
+# 2. By using Batch  Mode Approach
+
+a=10
+b=20
+c=a+b
+print(a)
+print(b)
+print(c)
+
+#Program for Mul two Numbers
+
+#MulEx.py
+
+a=float(input("Enter First Value:"))
+b=float(input("Enter Second Value:"))
+c=a*b
+print("*"*50)
+print("value of a={}".format(a))
+print("value of b={}".format(b))
+print("mul({},{})={}".format(a,b,c))
+print("*"*50)
+
+#program for Adding of Two Numbers
+
+a=38
+b=89
+c=293
+d=a+b+c
+print(a,b,c,d)
+
+a=38
+b=89
+c=293
+d=a+b+c
+print(a)
+print(b)
+print(c)
+print(d)
+
+a=38
+b=89
+c=293
+d=a+b+c
+print("*"*50)
+print("val of a=",a)
+print("val of b=",b)
+print("val of c=",c)
+print("val of d=",d)
+print("sum=",d)
+print("*"*50)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
