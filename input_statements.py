8d6254222e0ef691b2800756a9b89aa3ea32c735
Ajaykumar <pogulaajaykumar6801@gmail.com>
2025-09-05 22:58:49 +0530

    Add files via upload

---
+++ input_statements.py
@@ -0,0 +1,72 @@
+# -------------
+# 1. input()
+# ---------------
+# 2. input(Message)
+# -----------------
+
+a=input("enter your name")
+print("val of a={} and type={}".format(a,type(a)))
+
+print("enter any value")
+a=input()
+print("val of a={} and type{}".format(a,type(a)))
+
+
+o=input("enter any int val:")
+print("val of o={}".format(o,type(o)))
+k=input("enter any float value:")
+print("val of k={}".format(k,type(k)))
+
+
+x=float(input("enter any value:"))
+print("val of x={}".format(x,type(x)))
+
+        
+print("enter first value")
+c=float(input())
+print("enter second value")
+k=float(input())
+j=c+k
+print("sum of ({},{})".format(c,k,j))
+
+
+a=input("Enter Integer OR Float Value:")
+print("Val of a={} and type={}".format(a,type(a)))
+x=float(a)
+print("Val of x={} and type={}".format(x,type(x)))
+
+
+print("enter your first value:")
+o=float(input())
+print("enter your second value:")
+k=complex(input())
+l=o+k
+print("sum of ({},{})={}".format(o,k,l))
+
+
+v=float(input("enter your first value:"))
+l=float(input("enter your second value:"))
+h=v*l
+print("_"*50)
+print("\t\tval of v ={}".format(v))
+print("\t\tval of l ={}".format(l))
+print("\t\tsum ={}".format(h))
+print("_"*50)
+
+print("enter two values:")
+k=float(input())
+l=float(input())
+n=k+l
+print("\t\t{} +{} ={}".format(k,l,n))
+
+p=float(input("enter first value:"))
+k=float(input("enter second value:"))
+h=p*k
+print("\t\t{} * {} ={}".format(p,k,h))
+
+
+
+
+
+
+
